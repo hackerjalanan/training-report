b9d442ecd082592d99bc594b00539a68e3140b83
Hacker Jalanan <hackerjalanan>
2026-09-07 13:19:39 +0700

    update

--- config/db.config.js
+++ config/db.config.js
@@ -1,19 +1,19 @@
-//config/db.config.js
+// config/db.config.js
 module.exports = {
   mysql: {
-    HOST: process.env.MYSQL_HOST ,
-    PORT: process.env.MYSQL_PORT ,
-    USERNAME: process.env.MYSQL_USER ,
-    PASSWORD: process.env.MYSQL_PASS ,
-    DB: process.env.MYSQL_DB ,
+    HOST: process.env.MYSQL_HOST,
+    PORT: process.env.MYSQL_PORT,
+    USERNAME: process.env.MYSQL_USER,
+    PASSWORD: process.env.MYSQL_PASS,
+    DB: process.env.MYSQL_DB,
     DIALECT: "mysql",
+    dialectModule: require('mysql2'), 
     OPTIONS: {
       max: 5,
       min: 0,
       acquire: 30000,
       idle: 10000,
     },
-    LOGGING: process.env.NODE_ENV === "development" ? console.log : false, // Disable logging in production
+    LOGGING: process.env.NODE_ENV === "development" ? console.log : false,
   },
-
 };
